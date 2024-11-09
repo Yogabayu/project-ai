ed0b5687b3e97f1ad3e651ef86466cf90aaa46a6
Yoga Bayu AP <yogabayusbi@gmail.com>
2024-11-10 00:07:33 +0700

    v1.1|fix|updateurl

--- resources/js/axios.js
+++ resources/js/axios.js
@@ -2,9 +2,9 @@ import axios from 'axios';
 
 const mainURL = axios.create({
   /** localhost */
-  baseURL: 'http://localhost:8000/api/v1',
+  // baseURL: 'http://localhost:8000/api/v1',
   /** development */
-  // baseURL: 'https://ai.yogabayuap.com/api/v1',
+  baseURL: 'https://ai.yogabayuap.com/api/v1',
   /** production */
 });
 
